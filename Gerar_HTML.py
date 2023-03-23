66808baec718d378d55d9a71ac4f3f5ca8cf6a5c
AndersonxlD <aandersonpereira@gmail.com>
2023-03-22 21:51:19 -0300

    second commit

---
+++ Gerar_HTML.py
@@ -0,0 +1,54 @@
+import os
+import openpyxl
+import pandas as pd
+import warnings
+import datapane as dp
+import datapane.resources
+import datapane.resources.view_resources
+import datapane.resources.html_templates
+import datapane.resources.app_templates
+import plotly.express as px
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+warnings.filterwarnings('ignore')
+
+class Gerar:
+    def __init__(self):
+        self.desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+        self.titulo = '# Report Diario'
+        self.df = pd.read_excel(self.desktop_path+"\\airlines_delay.xlsx")
+
+    def CriarGRafico(self):
+        fig3 = px.scatter(self.df,x='Airline',y='Length',size='Length',color='DayOfWeek')
+        return fig3
+    
+    def CriarGRafico2(self):
+        plt.figure(figsize=(12,4))
+        fig3 = sns.boxplot(x='Airline', y='Length', data=self.df)
+        return fig3
+
+    def CriarTabela(self):
+        tabela = dp.DataTable(self.df)
+        return tabela
+    
+    def CriarTitulo(self):
+        texto = dp.Text(self.titulo)
+        return texto
+
+    def CriarApp(self):
+        tex = self.CriarTitulo()
+        graf = self.CriarGRafico()
+        graf2 = self.CriarGRafico2()
+        tab = self.CriarTabela()
+        app = dp.App(tex,graf,graf2,tab)
+        return app
+    
+    def Salvar(self):
+        app = self.CriarApp()
+        app.save(self.desktop_path+"\\Teste_Diario.html")
+
+gerar = Gerar()
+gerar.Salvar()
